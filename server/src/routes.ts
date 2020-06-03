347c1e0d396668eb591e03a584b0ab53999ddfbb
xXHachimanXx <globaldep90@gmail.com>
2020-06-02 23:30:25 -0300

    Criando rota para listar itens.

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
 
 /////////////////////////// REQUISIÇÕES ///////////////////////////
-routes.get("/", (req, res) => {
-    res.json({message: "Hello world"});
+routes.get("/itens", async (req, res) => {
+    const itens = await knex('itens').select('*');
+
+    const serializedItems = itens.map(item => ({
+        title: item.title,
+        image_url: `http://localhost:3333/uploads/${item.image}`,
+    }));
+
+    return res.json(serializedItems);
 });
+
+
 
 export default routes;

33b59a1d1c609285d620788b933be37f8a6f3ecd
xXHachimanXx <globaldep90@gmail.com>
2020-06-02 21:58:06 -0300

    Finalizando a criação de tabelas.

--- server/src/database/migrations/01_create_itens.ts
+++ server/src/database/migrations/01_create_itens.ts
@@ -1,7 +1,14 @@
-export async function up() {
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
     // CRIAR A TABELA
+    return knex.schema.createTable('itens', table => {
+        table.increments('id').primary();
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+    });
 }
 
-export async function down() {
-    // VOLTAR ATRÁS
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('itens');
 }
--- server/src/database/migrations/02_create_point_itens.ts
+++ server/src/database/migrations/02_create_point_itens.ts
@@ -1,7 +1,14 @@
-export async function up() {
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
     // CRIAR A TABELA
+    return knex.schema.createTable('point_itens', table => {
+        table.increments('id').primary();
+        table.integer('point_id').notNullable().references('id').inTable('points');
+        table.integer('item_id').notNullable().references('id').inTable('itens');
+    });
 }
 
-export async function down() {
-    // VOLTAR ATRÁS
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_itens');
 }

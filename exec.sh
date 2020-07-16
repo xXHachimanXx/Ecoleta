296989ef8ee14412b35f4a96c5e43a6d70022b02
xXHachimanXx <globaldep90@gmail.com>
2020-07-16 16:12:54 -0300

    Criando CLI para facilitar a execução do projeto.

--- exec.sh
+++ exec.sh
@@ -1,36 +1,76 @@
 #!/bin/bash
 function Web() {
 	cd ./web
-	npm install && npm start
+	npm start
 	cd ..
 }
 
 function Mobile() {
 	cd ./mobile
-	npm install && npm start
+	npm start
 	cd ..
 }
+
+function Server() {
+	cd ./server
+	npm run knex:migrate &&
+	npm run knex:seed &&
+	npm run dev
+	cd ..
+}
+
+function Atualizar() {
+	git pull;
+	clear;
+	npm install;
+	clear;
+
+	#Atualizar backend
+	cd ./server;
+	npm install;
+	cd ..;
+	
+	#Atualizar frontend
+	cd ./web;
+	npm install;
+	cd ..;
+
+	cd ./mobile;
+	npm install;
+	cd ..;
+}
+
+function Escolha() {
+	
+	echo "Qual versão do Ecoleta você deseja executar?"
+	echo "1- Web"
+	echo "2- Mobile"
+	echo "3- Server"
+	echo "4- Atualizar e instalar dependências"
+
+	echo "Digite um código : "
+	read VERSION
+	
+	case $VERSION in
+		1) Web ;;
+		2) Mobile ;;
+		3) Server ;;
+		4) Atualizar ;;
+		*) echo "Opção invalida!";;
+
+	esac
+}
+
+git pull && npm install;
 clear;
-git pull &&
-npm install;
-
-cd ./server;
-npx knex migrate:latest --knexfile knexfile.ts && run dev
-cd ..;
-
-echo "Qual versão do Ecoleta você deseja executar?"
-echo "Digite um código :"
-echo "1- Web"
-echo "2- Mobile"
-read VERSION
-
-case $VERSION in
-	1) Web ;;
-	2) Mobile ;;
+case $1 in
+	web) Web ;;
+	mobile) Mobile ;;
+	server) Server ;;
+	update) Atualizar ;;
+	"") Escolha ;;
 	*) echo "Opção invalida!";;
 esac
-
-
 
 
 
